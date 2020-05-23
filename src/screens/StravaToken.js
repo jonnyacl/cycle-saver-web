e64dbe554b32a169f2f44bc306b01cfda2b115c6
Jonny Lea <lea.jonny@gmail.com>
2020-05-23 11:47:21 +0100

    firebase gitignore

--- src/screens/StravaToken.js
+++ src/screens/StravaToken.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import { UserContext } from "../context/UserContext";
 import { extractQueries } from '../helpers';
@@ -7,44 +7,46 @@ import { StravaConnect } from '../components/StravaConnect';
 
 export const StravaToken = () => {
 
-    const [codeRequested, setCodeRequested] = useState(false);
-    const [stravaMessage, setStravaMessage] = useState("");
     const [userState, dispatch] = useContext(UserContext);
     const history = useHistory();
 
     const queries = extractQueries(history.location.search);
+
+    const failedToConnect = (eMessage) => {
+        return (
+            <div>
+                Failed to connect to Strava{eMessage}. Please try again if you wish to see how much you are saving (earning)!
+                <StravaConnect />
+            </div>
+        );
+    }
 
     if (queries.error) {
         let eMessage = `: ${queries.error}`;
         if (queries.error === "access_denied") {
             eMessage = ". You may have clicked cancel"
         }
-        return (
-            <div>
-                Failed to connect Strava{eMessage}. Please try again if you wish to see how much you are saving (earning)!
-                <StravaConnect />
-            </div>
-        );
-    } else if (queries.code && !codeRequested) {
+        return failedToConnect(eMessage);
+        
+    } else if (queries.code) {
         // obtain a token!
         console.log(`Obtaining a token with code ${queries.code}`);
         axios.post("strava/token", { code: queries.code, user_id: userState.user.uid }).then(resp => {
             console.log(`Connected to strava: ${JSON.stringify(resp.data)}`);
-            setStravaMessage("Connected to Strava.");
             dispatch({ type: "STRAVA_TOKEN_SUCCESS" });
-            setCodeRequested(true);
+            history.push("?status=success");
         }).catch(e => {
             console.error('Failed to obtain strava token', e);
             dispatch({ type: "STRAVA_TOKEN_FAIL" });
-            setCodeRequested(true);
+            history.push("?status=fail");
         });
-    } else if (codeRequested) {
-        setStravaMessage("Failed to connect Strava. Please try again if you wish to see how much you are saving (earning)!");
+    } else if (queries.status) {
+        if (queries.status === "success") {
+            return <div>Connected to Strava!</div>;
+        } else {
+            return failedToConnect();
+        }
     }
 
-    return (
-        <div>
-            {stravaMessage}
-        </div>
-    );
+    return null;
 };
